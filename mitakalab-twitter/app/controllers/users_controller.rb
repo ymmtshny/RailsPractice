b56160ee915ff57654ed178ade41fe4acf20611c
Shinya Yamamoto <yamashin1003@hotmail.co.jp>
2016-12-09 23:19:31 +0900

    railsのデータベースについて学びました。 schemaが枠組 migrateは印刷のようなもの seedは初期データ モデルの作成まで行いました。

--- mitakalab-twitter/app/controllers/users_controller.rb
+++ mitakalab-twitter/app/controllers/users_controller.rb
@@ -3,18 +3,6 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = Hash.new
-    if params[:username] == 'shinyamamoto'
-      @user[:name] = 'Shinya Yamamoto'
-      @user[:username] = 'shinyamamoto'
-      @user[:location] = 'Tokyo, JAPAN'
-      @user[:about] = 'Nice to meet you!!'
-    elsif params[:username] == 'hello'
-      @user[:name] = 'Helloさん'
-      @user[:username] = 'hello'
-      @user[:location] = 'Hello, JAPAN'
-      @user[:about] = 'Nice to meet you!!'
-    end
-
+    @user = User.find_by(:username => params[:username])
   end
 end
--- mitakalab-twitter/db/seeds.rb
+++ mitakalab-twitter/db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+@user = User.new
+@user.name = 'Shinya Yamamoto'
+@user.username = 'shinyamamoto'
+@user.location = 'Tokyo, JP'
+@user.about = 'Hello, my name is Shinya Yamamoto. Nice to meet you.'
+@user.save
+
+@user = User.new
+@user.name = 'Alex'
+@user.username = 'alex'
+@user.location = 'Riverside, CA'
+@user.about = 'Hello, my name is Alex. How are you doing today?'
+@user.save
