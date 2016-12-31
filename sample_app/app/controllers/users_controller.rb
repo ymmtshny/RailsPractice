2ae972f9da5da47d8f5cb126fb7b0c07145e9675
Shinya Yamamoto <yamashin1003@hotmail.co.jp>
2017-01-01 03:21:29 +0900

    7章終了

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -1,11 +1,33 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def show
     @user = User.find(params[:id])
-    debugger
+    # debugger
   end
+
+  def create
+    # @user = User.new(params[:user])
+    # これだとセキュリティ上問題があるので、以下のように
+    # user_paramsをprivateで渡す。
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name,
+                                   :email,
+                                   :password,
+                                   :password_confirmation)
+    end
 
 end
