832a587db4fdeee2b76bc24c5c5f6d4c8b092dc3
Shinya Yamamoto <yamashin1003@hotmail.co.jp>
2016-12-31 22:32:54 +0900

    ６章完了、テストが通らない問題を修正

--- sample_app/test/controllers/static_pages_controller_test.rb
+++ sample_app/test/controllers/static_pages_controller_test.rb
@@ -5,6 +5,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   def setup
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
--- sample_app/test/controllers/user_controller_test.rb
+++ sample_app/test/controllers/user_controller_test.rb
@@ -1,9 +1,15 @@
 require 'test_helper'
 
 class UserControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
   test "should get new" do
-    get user_new_url
+    get signup_url
     assert_response :success
+    assert_select "title", "Sign Up | #{@base_title}"
   end
 
 end
